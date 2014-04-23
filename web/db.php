9a2211a46d5d0dddaacd877b4fb1fdf510a5e2c2
avikarsha.saha <avikarsha.saha@gmail.com>
2014-04-23 19:54:26 +0530

    Update Db Function 2nd time

--- web/db.php
+++ web/db.php
@@ -5,94 +5,153 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+/* Add on 04-23-2014*/
 
+ function Db_Connect($db_server, $db_user, $db_password)
+	{
+		global $link;
+		
+		$link=mysql_connect($db_server, $db_user, $db_password) or die(mysql_error().'-Could not connect to server.');		
+		if($link)
+		{
+			$db=mysql_select_db(DB_NAME, $link) or die(mysql_error().'-Could not connect to database.');
+		}
+		return $link;		
+	}
+	
+	function Db_Close() 
+	{
+		global $link;
+		return mysql_close($link);
+	}
 
- function connect($address, $account, $pwd, $name) {
-        $this->_dbHandle = @mysql_connect($address, $account, $pwd);
-        if ($this->_dbHandle != 0) {
-            if (mysql_select_db($name, $this->_dbHandle)) {
-                return 1;
-            }
-            else {
-                return 0;
-            }
-        }
-        else {
-            return 0;
-        }
-    }
- 
-    /** Disconnects from database **/
- 
-    function disconnect() {
-        if (@mysql_close($this->_dbHandle) != 0) {
-            return 1;
-        }  else {
-            return 0;
-        }
-    }
-     
-    function selectAll() {
-        $query = 'select * from `'.$this->_table.'`';
-        return $this->query($query);
-    }
-     
-    function select($id) {
-        $query = 'select * from `'.$this->_table.'` where `id` = \''.mysql_real_escape_string($id).'\'';
-        return $this->query($query, 1);    
-    }
- 
-     
-    /** Custom SQL Query **/
- 
-    function query($query, $singleResult = 0) {
- 
-        $this->_result = mysql_query($query, $this->_dbHandle);
- 
-        if (preg_match("/select/i",$query)) {
-        $result = array();
-        $table = array();
-        $field = array();
-        $tempResults = array();
-        $numOfFields = mysql_num_fields($this->_result);
-        for ($i = 0; $i < $numOfFields; ++$i) {
-            array_push($table,mysql_field_table($this->_result, $i));
-            array_push($field,mysql_field_name($this->_result, $i));
-        }
- 
-         
-            while ($row = mysql_fetch_row($this->_result)) {
-                for ($i = 0;$i < $numOfFields; ++$i) {
-                    $table[$i] = trim(ucfirst($table[$i]),"s");
-                    $tempResults[$table[$i]][$field[$i]] = $row[$i];
-                }
-                if ($singleResult == 1) {
-                    mysql_free_result($this->_result);
-                    return $tempResults;
-                }
-                array_push($result,$tempResults);
-            }
-            mysql_free_result($this->_result);
-            return($result);
-        }
-         
- 
-    }
- 
-    /** Get number of rows **/
-    function getNumRows() {
-        return mysql_num_rows($this->_result);
-    }
- 
-    /** Free resources allocated by a query **/
- 
-    function freeResult() {
-        mysql_free_result($this->_result);
-    }
- 
-    /** Get error string **/
- 
-    function getError() {
-        return mysql_error($this->_dbHandle);
-    }
+	function Select_Qry($fields,$table,$where_clause,$orderby,$type,$startRow, $PageSize)
+	{
+			$sql_condition="";
+			if($where_clause!="")
+			{
+				$sql_condition.=" WHERE ".$where_clause;
+			}
+			if($orderby!='')
+			{
+				$sql_condition.=" ORDER BY ".$orderby;
+			}
+			if($orderby !='' && $type!='')
+			{
+				$sql_condition.=" ".$type;
+			}
+			if($startRow!='' && $PageSize!='')
+			{
+				$sql_condition.=" LIMIT ".$startRow.",".$PageSize;
+			}
+			//echo "SELECT ".$fields." FROM ".$table." ".$sql_condition."".'<br>';
+			$resCondition = mysql_query("SELECT ".$fields." FROM ".$table." ".$sql_condition."")or die(mysql_error()."Error in selectQry() ");
+			if(mysql_num_rows($resCondition) > 0)
+				{
+					$obCondition = mysql_fetch_array($resCondition);
+					$fields="";
+					$table="";
+					$where_clause="";
+					$orderby="";
+					$type="";
+					#print_r($obCondition);
+					
+					return $obCondition;
+				}
+			else
+			{
+					$fields="";
+					$table="";
+					$where_clause="";
+					$orderby="";
+					$type="";
+				    return false;
+			}
+	}
+		
+	function Listing_Qry($field,$table,$where_clause)
+	{
+		if($where_clause != "")
+		{
+			//echo "SELECT ".$field." FROM ".$table." ".$where_clause."<br/>";
+			$ListContent = mysql_query("SELECT ".$field." FROM ".$table." ".$where_clause."") or die(mysql_error()."Error in listingQry()");
+		}
+		else
+		{
+			$ListContent = mysql_query("SELECT ".$field." FROM ".$table) or die(mysql_error()."Error in listingQry()");
+		}
+		if(mysql_num_rows($ListContent) > 0)
+		{
+			while($objContent = mysql_fetch_array($ListContent))
+			{
+				$arrList[] = $objContent;
+			}
+			return $arrList;
+		}
+		else
+		{
+			return 0;
+		}
+	}
+
+	function Delete_Qry($table,$where_clause)
+	{
+		mysql_query("DELETE FROM ".$table." ".$where_clause."") or die(mysql_error()."Error in deleteQry()");
+		//print "DELETE FROM ".$table." ".$where_clause."";
+		return true;
+	}
+	
+	function Truncate_Qry($table)
+	{
+		mysql_query("TRUNCATE TABLE ".$table."") or die(mysql_error()."Error in truncateQry()");
+		//print "TRUNCATE TABLE ".$table."";
+		//exit;
+		return true;
+	}
+
+	function Insert_Qry($table,$value)
+	{
+		//print "INSERT INTO ".$table." SET ".$value."";
+		//exit;
+		mysql_query("INSERT INTO ".$table." SET ".$value."") or die(mysql_error()."Error in insertQry()");
+		return mysql_insert_id();
+	}
+
+	function Update_Qry($table, $set_value, $where_clause)
+	{
+		//print "<br>UPDATE ".$table." SET ".$set_value." ".$where_clause."<br>";
+		//exit;
+		mysql_query("UPDATE ".$table." SET ".$set_value." ".$where_clause."") or die(mysql_error()."Error in updateQry()");
+		return true;
+	}
+	
+	function Advance_Qry($query = '', $multi = false)// $multi = true if you want to return multipile rows
+	{
+		global $link;
+		//print $query;
+		
+		$resultSet = array();
+		$result    = mysql_query($query, $link) or die(mysql_error($link)."Error in advanceQry()");
+		
+		if(mysql_num_rows($result) > 0)
+		{
+			if($multi == false)
+			{
+				return mysql_fetch_array($result);
+			}
+			else
+			{
+				while($list = mysql_fetch_array($result))
+				{
+					$resultSet[] = $list;
+				}
+				
+				return $resultSet;
+			}
+		}
+		else
+		{
+			return false;
+		}
+	}
 ?>
